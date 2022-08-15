cd637a25a5979f2fc534957d854d94862fc22392
Илюша <79216023478@yandex.ru>
2022-08-15 18:09:16 +0300

    feat: добавить TestController, удивиться, как много spring boot делает под капотом

---
+++ src/main/java/ru/practicum/laterwithspringboot/LaterWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.laterwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaterWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaterWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/practicum/laterwithspringboot/LaterWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.laterwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaterWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
